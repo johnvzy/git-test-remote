7062b45e1d5503f4e30de612143218c3286dd70d
johnv <kiyoto_zy@hotmail.com>
2020-07-31 11:50:21 +0800

    only path

--- abc.ts
+++ abc.ts
@@ -1,5 +1,1 @@
-console.log("Hello World");
-console.log("Hello World B");
-console.log("Hello World C");
-console.log("Hello World XYZ");
-console.log("Hello World Z");
+console.log("Hello World Path2");
